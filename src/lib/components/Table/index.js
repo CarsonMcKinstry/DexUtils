6fcca99dd418f6489101ac42b68e417541ee3a4a
Carson McKinstry <carson.mckinstry@gmail.com>
2018-06-13 16:31:23 -0500

    renamed the pagination component

--- src/lib/components/Table/index.js
+++ src/lib/components/Table/index.js
@@ -1,7 +1,7 @@
 export { default as Table } from './Table';
+export { default as TablePagination } from './TablePagination';
 export { default as TableHeader } from './TableHeader';
 export { default as TableLink } from './TableLink';
 export { default as TableRow } from './TableRow';
 export { default as TableCell } from './TableCell';
 export { default as RecordCell } from './RecordCell';
-export { default as Pagination } from './Pagination';
